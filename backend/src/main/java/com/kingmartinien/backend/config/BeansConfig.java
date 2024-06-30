8163dbc3bb7c465f98eddaeaeb650f41c8204969
king-martinien <martinienfokoue@yahoo.com>
2024-06-30 08:01:06 +0100

    Add -> CORS configuration

--- backend/src/main/java/com/kingmartinien/backend/config/BeansConfig.java
+++ backend/src/main/java/com/kingmartinien/backend/config/BeansConfig.java
@@ -1,9 +1,12 @@
 package com.kingmartinien.backend.config;
 
 import com.kingmartinien.backend.auditing.AuditorAwareImpl;
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BeansConfig {
@@ -11,6 +14,20 @@ public class BeansConfig {
     @Bean
     AuditorAware<String> auditorAware() {
         return new AuditorAwareImpl();
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200,http://127.0.0.1:5500")
+                        .allowedMethods("*")
+                        .allowCredentials(true)
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 }
